628bbd0be55386377402e7c26d50100de5f31df5
Jose <38859080+FULLRAIVA@users.noreply.github.com>
2019-12-08 09:59:56 -0300

    Add files via upload
    Menus colocados e mais uma pagina feita, falta uma

--- lib/pages/SearchPageThree.dart
+++ lib/pages/SearchPageThree.dart
@@ -25,6 +25,244 @@ class _SearchPageThreeState extends State<SearchPageThree> {
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
           ),
 
+
+          drawer: Drawer(
+            child:Container(
+              color:Color(0xffa4002c) ,
+              child: ListView(
+                children: <Widget>[
+                  DrawerHeader(
+                      decoration: BoxDecoration(
+
+                        color:  Color(0xffa4002c),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+
+                            height: 100,
+                            alignment: Alignment.bottomLeft,
+                            child: Text("Bem Vindo, Whinderson",
+                              style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold, fontSize: 27),),
+                          ),
+
+
+                        ],
+                      )
+                  ),
+
+                  ListTile(
+
+                    title:Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              width: 30,
+
+                              child: FloatingActionButton(
+                                backgroundColor: Colors.white,
+
+                              ),
+                            ),
+
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Indique um amigo",
+                                style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold, fontSize: 22,fontStyle: FontStyle.italic),),
+                            )
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                    //onTap: Aqui a gente coloca a função que esse texto vai fazer,
+                  ),
+
+                  ListTile(
+
+                    title:Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              width: 30,
+
+                              child: FloatingActionButton(
+                                backgroundColor: Colors.white,
+
+                              ),
+                            ),
+
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Endereços",
+                                style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold, fontSize: 22,fontStyle: FontStyle.italic),),
+                            )
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                    //onTap: Aqui a gente coloca a função que esse texto vai fazer,
+                  ),
+
+                  ListTile(
+
+                    title:Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              width: 30,
+
+                              child: FloatingActionButton(
+                                backgroundColor: Colors.white,
+
+                              ),
+                            ),
+
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Solicitações",
+                                style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold, fontSize: 22,fontStyle: FontStyle.italic),),
+                            )
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                    //onTap: Aqui a gente coloca a função que esse texto vai fazer,
+                  ),
+                  ListTile(
+
+                    title:Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(left: 50),
+                              width: 20,
+
+                              child: FloatingActionButton(
+                                backgroundColor: Colors.white,
+
+                              ),
+                            ),
+
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Em andamento",
+                                style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold, fontSize: 17,fontStyle: FontStyle.italic),),
+                            )
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                    //onTap: Aqui a gente coloca a função que esse texto vai fazer,
+                  ),
+                  ListTile(
+
+                    title:Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(left: 50),
+                              width: 20,
+
+                              child: FloatingActionButton(
+                                backgroundColor: Colors.white,
+
+                              ),
+                            ),
+
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Concluídos",
+                                style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold, fontSize: 17,fontStyle: FontStyle.italic),),
+                            )
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                    //onTap: Aqui a gente coloca a função que esse texto vai fazer,
+                  ),
+
+                  ListTile(
+
+                    title:Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              width: 30,
+
+                              child: FloatingActionButton(
+                                backgroundColor: Colors.white,
+
+                              ),
+                            ),
+
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Minhas Infos",
+                                style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold, fontSize: 22,fontStyle: FontStyle.italic),),
+                            )
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                    //onTap: Aqui a gente coloca a função que esse texto vai fazer,
+                  ),
+                  ListTile(
+
+                    title:Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              width: 30,
+
+                              child: FloatingActionButton(
+                                backgroundColor: Colors.white,
+
+                              ),
+                            ),
+
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text("Configurações",
+                                style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold, fontSize: 22,fontStyle: FontStyle.italic),),
+                            )
+
+                          ],
+                        )
+
+                      ],
+                    ),
+                    //onTap: Aqui a gente coloca a função que esse texto vai fazer,
+                  ),
+
+
+
+                ],
+              ),
+            ),
+
+
+          ),
+
+
           backgroundColor: Color(0xffa4002c),
           body: Padding(
             padding: const EdgeInsets.all(70.0),
@@ -250,17 +488,7 @@ class _SearchPageThreeState extends State<SearchPageThree> {
 
           ),
         ),
-        Positioned(
-            top: kToolbarHeight,
-            bottom: 689,
-            left: 15,
 
-            child: FloatingActionButton(
-
-              child: Icon(Icons.dehaze, size: 30,),
-              backgroundColor: Color.fromRGBO(80, 0, 0, 1),
-            )
-        )
 
 
       ],
